2e90ebf8000d525c0126cdb0b331f5d1d46f2de1
perry <perry@pc>
2020-11-01 08:52:22 +0000

    Add passive mode and logging

--- Helpers/Ftp.php
+++ Helpers/Ftp.php
@@ -4,7 +4,7 @@ namespace Prymag\OrdersExporter\Helpers;
 
 use Magento\Framework\Filesystem\Driver\File;
 use Magento\Framework\Filesystem\Io\Ftp as FtpClient;
-
+use Psr\Log\LoggerInterface;
 
 class Ftp {
 
@@ -12,25 +12,45 @@ class Ftp {
 
     protected $_ftpClient;
 
+    protected $logger;
+
     public function __construct(
         File $file,
-        FtpClient $ftpClient
+        FtpClient $ftpClient,
+        LoggerInterface $logger
     )
     {
-        # code...
+        $this->logger = $logger;
         $this->_file = $file;
         $this->_ftpClient = $ftpClient;
     }
 
     public function send($attachments, $args = [])
     {
-        # code..
+        $args['passive'] = 1; // Force Passive for now
         $this->_ftpClient->open($args);
 
         if (count($attachments)) {
+            $this->logger->info("Sending files via ftp...");
             foreach ($attachments as $attachment) {
                 $content = $this->_file->fileGetContents($attachment['full_path']);
-                $this->_ftpClient->write($attachment['filename'], $content);
+
+                $retries = 1;
+
+                while ($retries < 5) {
+                    try {
+                        $success = $this->_ftpClient->write($attachment['filename'], $content);
+                        if ($success) {
+                            break;
+                        } else {
+                            $this->logger->notice('Unable to send file: ' . $attachment['filename']);
+                            $retries++;
+                        }
+                    } catch (\Exception $e) {
+                        $retries++;
+                        $this->logger->error("Exception sending file: " . $attachment['filename'] . ' ' . $e->getMessage());
+                    }
+                }
             }
         }
 
